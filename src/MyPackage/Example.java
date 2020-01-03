3cf2531f0455d0c29a76d2f0d3f2f4ed628f441c
punyasloka <punyasloka@versa-networks.com>
2020-01-03 13:02:19 +0530

    comments added

--- src/MyPackage/Example.java
+++ src/MyPackage/Example.java
@@ -1,6 +1,9 @@
 package MyPackage;
-
+/*
+This is class: Example
+ */
 public class Example {
+    // main function starts here
     public static void main(String[] args) {
         System.out.println("punyasloka pradhan");
     }
