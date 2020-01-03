187179221370b615cf7b2b0020f6cfc662e81cbd
punyasloka <punyasloka@versa-networks.com>
2020-01-03 13:22:40 +0530

    For usage example

---
+++ src/MyPackage/ForTest.java
@@ -0,0 +1,10 @@
+package MyPackage;
+
+public class ForTest {
+    public static void main(String[] args) {
+
+        for(int x = 0; x < 10; x = x + 1) {
+            System.out.println("current value of x : " + x);
+        }
+    }
+}
