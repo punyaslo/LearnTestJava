3d9e8c2d28639be4dfb37cfac5930460a46c2cdb
punyasloka <punyasloka@versa-networks.com>
2020-01-03 13:24:21 +0530

    For usage example - comments added

--- src/MyPackage/ForTest.java
+++ src/MyPackage/ForTest.java
@@ -3,6 +3,10 @@ package MyPackage;
 public class ForTest {
     public static void main(String[] args) {
 
+        /*
+        Demonstrate the for loop.
+        Call this file "ForTest.java".
+        */
         for(int x = 0; x < 10; x = x + 1) {
             System.out.println("current value of x : " + x);
         }
